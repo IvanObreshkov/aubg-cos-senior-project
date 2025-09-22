7176f7911afaa6b5e3268bed8f813f0f13b5700e
Ivan Obreshkov <ivanobreshkov12@gmail.com>
2025-09-22 12:42:18 +0300

    Fix import for LogEntry

--- internal/raft/server/state_machine.go
+++ internal/raft/server/state_machine.go
@@ -1,9 +1,11 @@
 package server
 
+import "aubg-cos-senior-project/internal/raft/transport"
+
 // StateMachine is an interface representing the StateMachine of the Server defined in Section 2 from the
 // [Raft paper](https://raft.github.io/raft.pdf). It is inspired from the FSM interface defined in
 // [Hashicorp's Raft impl](https://github.com/hashicorp/raft/blob/main/fsm.go)
 type StateMachine interface {
-	Apply(log []LogEntry)
+	Apply(log []transport.LogEntry)
 	// TODO: Add Snapshot and Restore methods
 }
